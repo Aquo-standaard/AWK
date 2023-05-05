6058c7ef13bd56c2904047ae3bac4cb3ea3bf056
Eclidian <w.vanriel@ihw.nl>
2023-05-05 10:37:38 +0200

    update config.js

--- config.js
+++ config.js
@@ -1,37 +1,147 @@
-var respecConfig = {
-  specStatus: "",
-  specType: "IM",
-  //subtitle: "IMWA gegevenscatalogus",
-   //publishDate: "2023-04-26", 
-  logos: [
-  {src: 'https://www.aquo.nl//logos/aquo_logo.svg',
-    href: "http://aquo.nl",
-    alt: "Aquo",
-    width: 252,
-    height: 130,
-    id: 'AWK'},
-  {
-    src: 'https://cuatro.sim-cdn.nl/ihw/uploads/styles/logo/media/ihw_logo_def_taglines.png?cb=ZxTlKJiY',
-    href: "https://www.ihw.nl",
-    alt: "Informatiehuis Water",
-    width: 229,
-    height: 130,
-    id: 'IHW',
-  }]  ,
-  editors: [],
-  authors: [
+//-------------------------------------------------------------------------------------
+//-- File. . . :  config.js
+//-- Bevat . . :  Template voor de  configuratie voor respec  
+//--              Gebaseerd op https://github.com/aquo/respec/wiki
+//--              Deze file moet worden neergezet in de root-directory van de 
+//--              betreffende standaard. 
+//-- Door. . . :  Wijnand van Riel
+//-------------------------------------------------------------------------------------
+
+var respecConfig = 
+{
+  //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
+  //specStatus: "GN-BASIS",             // Basis Document
+  specStatus: "GN-WV",              // Werk Versie
+  //specStatus: "GN-CV",              // Consultatie Versie
+  //specStatus: "GN-VV",              // Vaststellings Versie
+  //specStatus: "GN-DEF",             // Definitieve Versie
+  //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
+  //specType: "NO",                   // Norm
+  specType: "ST",                   // Standaard
+  //specType: "IM",                   // Informatie Model
+  //specType: "PR",                   // Praktijkrichtlijn
+  //specType: "HR",                   // HandReiking
+  //specType: "WA",                   // Werkafspraak
+  //specType: "BD",                   // Beheer Documentatie
+  //-- format is verplicht! -----------------------------------------------------------
+  //format: "markdown",                 // altijd "markdown" - JvG W3C aangepast, op 1 plek markdown aangeven
+format: "html",                 // altijd "markdown" - JvG W3C aangepast, op 1 plek markdown aangeven     
+  //-- publishDate is verplicht -------------------------------------------------------
+  //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
+  //publishDate: "2019-10-08",  	    // Format is "YYYY-MM-DD"
+  //-- Repositorynaam op GitHub -------------------------------------------------------
+  github: "https://github.com/aquo/<naam van de repository>",
+  //-- Repositorynaam/issues op GitHub ------------------------------------------------
+  issueBase: "https://github.com/aquo/<naam van de repository>/issues",
+  //-- edDraftURI: de URI van de werkversie van het document
+  edDraftURI: "https://github/aquo/<naam van de repository>",
+  //-- de namen van de Editor(s) ------------------------------------------------------
+  //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
+  editors: 
+ [
     {
-      name: "Informatiehuis Water",
-	  mailto: "servicedesk@ihw.nl",
-	  note: "033 203 50 97",
-      companyURL: "http://www.ihw.nl",
-    }],
-  shortName: "AWK",
-  pubDomain: "IMWA",
-  github: "https://github.com/Aquo-standaard/AWK",
-  previousPublishDate: "2023-04-26",
-  previousStatus: "AQ-WV",
-  localBiblio: {
-    
+      name:       "Naam Editor-1",
+      company:    "aquo",
+      companyURL: "https://www.aquo.nl"
+    }, 
+    {
+      name:       "Naam Editor-n",
+      company:    "aquo",
+      companyURL: "https://www.aquo.nl"
+    }, 
+  ],
+  //-- de namen van de Author(s) ------------------------------------------------------
+  //-- vul in: per Author: name:, company:, companyURL: -------------------------------
+  authors: 
+  [
+    {
+      name:       "Naam Auteur-1",
+      company:    "aquo",
+      companyURL: "https://www.aquo.nl"
+    }, 
+    {
+      name:       "Naam Auteur-2",
+      company:    "aquo",
+      companyURL: "https://www.aquo.nl"
+    }, 
+  ],
+  //-- shortName is verplicht! (komt in de URL: kies logische naam) --------------------
+  shortName: "AquoLD",  	              // Wordt gebruikt in de document URL
+  //-- pubDomain is verplicht! (komt in de URL: Activeer 1 van de volgende) ------------
+  pubDomain: "AquoLD", 	              // Energie transitie
+  //pubDomain: "mim", 	            // Metamodel Informatie Modellering
+  //pubDomain: "bor", 	            // Beheer Openbare Ruimte
+  //pubDomain: "bro", 	            // Basisregistratie Ondergrond
+  //pubDomain: "imgeo", 	          // IMGeo / BGT
+  //pubDomain: "kl", 	              // Kabels en Leidingen
+  //pubDomain: "liv", 	            // Landelijke Informatievoorziening Vastgoedgebruik
+  //pubDomain: "md", 	              // Metadata
+  //pubDomain: "nen3610", 	        // Basismodel NEN3610
+  //pubDomain: "oov", 	            // Openbare Orde en Veiligheid
+  //pubDomain: "ro", 	              // Ruimtelijke Ordening
+  //pubDomain: "serv", 	            // Services
+  //pubDomain: "visu", 	            // Visualisatie
+  //pubDomain: "wp", 	              // White Paper
+  //-- license: voor de geldende gebruiksvoorwaarden
+  //licence: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
+  //licence: "cc0",                 // Public Domain Dedication
+  licence: "cc-by",                 // Attribution, met bronvermelding
+  
+  //-- localBiblio: lokale bibliografie, voor verwijzigingen
+  //-- NB: kijk eesrt naar de beschikbare www.specref.org voor verwijziging 
+  localBiblio: 
+  {
+    "PUB-1":
+      {
+        title:      "Titel van Publicatie-1",
+        href:       "https://www.aquo.nl",
+        status:     "V1.0.1",
+        publisher:  "Publisher-1",
+        company:    "Companynaam",
     },
+    "PUB-2":
+      {
+        title:      "Titel van Publicatie-2",
+        href:       "https://www.aquo.nl",
+        status:     "V1.0.1",
+        publisher:  "Publisher-2",
+        company:    "Companynaam",
+    },
+    "PUB-3":
+      {
+        title:      "Titel van Publicatie-3",
+        href:       "https://www.aquo.nl",
+        status:     "V1.0.1",
+        publisher:  "Publisher-3",
+        company:    "Companynaam",
+    },
+    "PUB-4":
+      {
+        title:      "Titel van Publicatie-4",
+        href:       "https://www.aquo.nl",
+        status:     "V1.0.1",
+        publisher:  "Publisher-4",
+        company:    "Companynaam",
+    },
+  },
+  postProcess:[custGHPG],   //-- Optioneel voor een multi document repository
+
+  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
+  //previousPublishDate: "2018-09-18",  	    // Format is "YYY-MM-DD"
+  //previousMaturity: "CV",                   // kies 1 van deze 2 regels  	  
+  //previousMaturity: "VV",  	                // kies 1 van deze 2 regels
+
+  //-- Optionele parameters:
+  //emailComments: "mim@aquo.nl",         // reactie mailadres, alleen bij CV!
+  //subtitle: "iets",                         // Subtitel van het document
+  //maxTocLevel: 3,                           // Aantal niveau's ToC, default is 0
+  //-- LOGO: Hier kan je een ander logo opgeven indien nodig
+  //logos: [{
+  //  src: "https://tools.geostandaarden.nl/respec/style/logos/OmgevingswetLogo.svg",
+  //  alt: "Standaarden Omgevingswet",
+  //  id: "TopLogo",
+  //  height: 67,
+  //  width: 300,
+  //  url: "https://www.aquo.nl/geo-standaarden/omgevingswet/STOPTPOD"
+  // }],
 };
